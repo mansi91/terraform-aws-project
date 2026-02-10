9c20ab400a95bc5fc512a3019fe2447eb50d1679
mansi91 <mona.uikey@gmail.com>
2026-02-10 12:42:38 +0530

    Initial Terraform configuration with EC2 instance

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-west-2"  # Change to your preferred region
+}
+
+# Data source to find the latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+  
+  owners = ["099720109477"]  # Canonical
+}
+
+# Create an EC2 instance
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"    # Free tier eligible
+  
+  tags = {
+    Name = "terraform-instance"
+  }
+}
