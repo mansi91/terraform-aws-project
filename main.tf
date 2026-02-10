5ca9f88af3ec4707810b5cb90f2725d176b2096b
mansi91 <mona.uikey@gmail.com>
2026-02-10 13:20:03 +0530

    Update EC2 instance type to t3.micro for better performance

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 # Create an EC2 instance
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"    # Free tier eligible
+  instance_type = "t3.micro"    # Free tier eligible
   
   tags = {
     Name = "terraform-instance"
