981611930b2002d6e23e06593a245ada16996a55
Mona Uikey <mona.uikey@gmail.com>
2026-02-10 22:49:41 +0530

    Modify AWS instance configuration in main.tf
    Updated instance tags and added data source for AWS AMI.

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
-# FROM:
-tags = {
-    Name = "terraform-instance"
+provider "aws" {
+  region = "us-west-2"
 }
 
-# TO:
-tags = {
-    Name = "terraform-instance-updated"
-    Environment = "dev"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+  
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "terraform-instance"
+  }
 }
